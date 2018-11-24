4742752ed09f086c9e680f24e2ae31b30fa21dd8
SkySmith2 <daniel.mulley@Outlook.com>
2018-11-24 00:24:37 +0000

    Finished off Main (without config generation)

--- src/main/java/sky/mulley/DiscordConnect/DiscordConnect.java
+++ src/main/java/sky/mulley/DiscordConnect/DiscordConnect.java
@@ -1,4 +1,36 @@
 package sky.mulley.DiscordConnect;
 
-public class DiscordConnect {
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+import sky.mulley.DiscordConnect.Commands.MessageCore;
+import sx.blah.discord.api.ClientBuilder;
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.handle.obj.IChannel;
+
+public class DiscordConnect extends JavaPlugin {
+    private static String token;
+    private IDiscordClient client;
+    private static IChannel textChannel;
+    private static IChannel adminChannel;
+
+    @Override
+    public void onEnable() {
+        //Get all the config file grabs here
+        client = buildDiscordClient(token);
+        client.getDispatcher().registerListener(new MessageCore());
+        client.login();
+        Bukkit.getLogger().info("[DiscordConnect] We have Discord and are logged in!");
+    }
+
+    @Override
+    public void onDisable() {
+        client.logout();
+    }
+
+    private static IDiscordClient buildDiscordClient(String token) {
+        return new ClientBuilder()
+                .withToken(token)
+                .withRecommendedShardCount()
+                .build();
+    }
 }
