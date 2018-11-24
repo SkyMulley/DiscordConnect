df5eb104813b760232cbeadd40fabf5494d35e64
SkySmith2 <daniel.mulley@Outlook.com>
2018-11-24 11:05:21 +0000

    Extension of previous commit

--- src/main/java/sky/mulley/DiscordConnect/MessageListener.java
+++ src/main/java/sky/mulley/DiscordConnect/MessageListener.java
@@ -1,16 +1,22 @@
 package sky.mulley.DiscordConnect;
 
+import org.bukkit.Bukkit;
 import sky.mulley.DiscordConnect.Commands.CommandCore;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
 
 public class MessageListener {
     private CommandCore cc;
+    private MessageReceivedEvent eventMsg;
+    DiscordMsgEvent msgEvent = new DiscordMsgEvent(eventMsg);
+
     public MessageListener(CommandCore cc) { this.cc =cc;}
+
     @EventSubscriber
     public void onMessageRecieved(MessageReceivedEvent event) {
+        eventMsg = event;
         if(!cc.commandCheck(event)) {
-            //Add more custom stuff here, not so sure
+            Bukkit.getPluginManager().callEvent(msgEvent);
         }
     }
 }
