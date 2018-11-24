f9cbfa3655a81a4de1d13d60713df16cd441d6fc
SkySmith2 <daniel.mulley@Outlook.com>
2018-11-24 00:39:15 +0000

    Added CommandCore for general command manipulation

---
+++ src/main/java/sky/mulley/DiscordConnect/Commands/CommandCore.java
@@ -0,0 +1,40 @@
+package sky.mulley.DiscordConnect.Commands;
+
+import org.bukkit.Bukkit;
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+
+import java.util.List;
+
+public class CommandCore {
+    private List<BaseCommand> commandList;
+    private String BOT_PREFIX;
+    private boolean hasStarted = false;
+    public CommandCore(String BOT_PREFIX) {
+        this.BOT_PREFIX = BOT_PREFIX;
+    }
+
+    public void addCommand(BaseCommand command) {commandList.add(command);}
+    public void removeCommand(BaseCommand command) {commandList.remove(command);}
+    public List<BaseCommand> getCommands() {return commandList;}
+
+    public boolean commandCheck(MessageReceivedEvent event) {
+        if(!hasStarted) { setupCommands(); hasStarted = false; }
+        String[] argArray = event.getMessage().getContent().split(" ");
+        if (argArray.length == 0 || !argArray[0].startsWith(BOT_PREFIX)) {
+            return false;
+        }
+        String commandStr = argArray[0].substring(BOT_PREFIX.length());
+        for (BaseCommand command : commandList) {
+            if(commandStr.toLowerCase().contains(command.getCommandName().toLowerCase())) {
+                Bukkit.getLogger().info("[DiscordConnect] " + event.getAuthor().getName() + " ran command " + command.getCommandName());
+                command.executeCommand(event, argArray);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void setupCommands() {
+
+    }
+}
