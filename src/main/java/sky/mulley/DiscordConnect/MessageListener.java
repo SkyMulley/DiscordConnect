8c455a4e5fd70d2380618cb66046aa60efa6f7d0
SkySmith2 <daniel.mulley@Outlook.com>
2018-11-24 17:08:52 +0000

    Starting off setting up the server integration

--- src/main/java/sky/mulley/DiscordConnect/MessageListener.java
+++ src/main/java/sky/mulley/DiscordConnect/MessageListener.java
@@ -1,12 +1,15 @@
 package sky.mulley.DiscordConnect;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import sky.mulley.DiscordConnect.Commands.CommandCore;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import sx.blah.discord.handle.obj.IChannel;
 
 public class MessageListener {
     private CommandCore cc;
+    private IChannel channel = null;
     private MessageReceivedEvent eventMsg;
     DiscordMsgEvent msgEvent = new DiscordMsgEvent(eventMsg);
 
@@ -17,6 +20,10 @@ public class MessageListener {
         eventMsg = event;
         if(!cc.commandCheck(event)) {
             Bukkit.getPluginManager().callEvent(msgEvent);
+        } if (channel!=null && event.getChannel().equals(channel)) {
+            Bukkit.getServer().broadcastMessage(ChatColor.BLUE+"[Discord] "+ChatColor.WHITE+event.getAuthor().getName()+":"+event.getMessage().toString());
         }
     }
+
+    public void setChannel(IChannel channel) { this.channel=channel;}
 }
---
+++ src/main/java/sky/mulley/DiscordConnect/ServerSync.java
@@ -0,0 +1,37 @@
+package sky.mulley.DiscordConnect;
+
+import com.vdurmont.emoji.Emoji;
+import com.vdurmont.emoji.EmojiManager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import sx.blah.discord.handle.obj.IChannel;
+
+public class ServerSync implements org.bukkit.event.Listener {
+    private DiscordConnect plugin;
+    private IChannel channel;
+    private Emoji plus = EmojiManager.getForAlias("heavy_plus_sign");
+    private Emoji minus = EmojiManager.getForAlias("heavy_minus_sign");
+    private Emoji skull = EmojiManager.getForAlias("skull");
+    public ServerSync(DiscordConnect plugin, IChannel channel) {
+        this.plugin = plugin;
+        this.channel = channel;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        channel.sendMessage(plus+event.getJoinMessage());
+    }
+
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent event) {
+        channel.sendMessage(minus+event.getQuitMessage());
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        channel.sendMessage(skull+event.getDeathMessage());
+    }
+}
