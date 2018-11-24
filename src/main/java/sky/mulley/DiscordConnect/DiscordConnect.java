306d4a7fda6b7100cc3a04c38b84c4acdfeaa027
SkySmith2 <daniel.mulley@Outlook.com>
2018-11-24 11:06:07 +0000

    Added a MC Channel, Admin Channel and Guild Check

--- src/main/java/sky/mulley/DiscordConnect/DiscordConnect.java
+++ src/main/java/sky/mulley/DiscordConnect/DiscordConnect.java
@@ -6,12 +6,14 @@ import sky.mulley.DiscordConnect.Commands.CommandCore;
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IGuild;
 
 public class DiscordConnect extends JavaPlugin {
     private String token;
     private IDiscordClient client;
     private IChannel textChannel;
     private IChannel adminChannel;
+    private IGuild guild;
     private String BOT_PREFIX;
     private CommandCore cc;
 
@@ -23,6 +25,7 @@ public class DiscordConnect extends JavaPlugin {
         client.getDispatcher().registerListener(new MessageListener(cc));
         client.login();
         Bukkit.getLogger().info("[DiscordConnect] We have Discord and are logged in!");
+        checkStatus();
     }
 
     @Override
@@ -38,4 +41,16 @@ public class DiscordConnect extends JavaPlugin {
     }
 
     public CommandCore getCommandCore() { return cc;}
+
+    public IDiscordClient getBotClient() { return client;}
+
+    private void checkStatus() {
+        Boolean statusNotMet = true;
+        Boolean adminFound = false;
+        Boolean guildFound = false;
+        for(IGuild guilds : client.getGuilds()) { if(guilds.equals(guild)) { guildFound = true;} }
+        if(guildFound = true) { for(IChannel channel : client.getGuildByID(guild.getLongID()).getChannels()) { if(channel.equals(textChannel)) { statusNotMet = false; } if (channel.equals(adminChannel)) { adminFound = true; } } }
+        if(statusNotMet=true) { Bukkit.getLogger().info("[DiscordConnect] The specified channel for Minecraft Chat was not found! Disabling this function"); textChannel = null;}
+        if(adminFound=false) { Bukkit.getLogger().info("[DiscordConnect] No Admin Console Channel was found, disabling this function."); adminChannel = null;}
+    }
 }
