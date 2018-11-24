d1f16816a863f7aec16d8a7e96d60bf749fc9633
SkySmith2 <daniel.mulley@Outlook.com>
2018-11-24 00:47:04 +0000

    Updated BaseCommand with new "helpViewable" setting

--- src/main/java/sky/mulley/DiscordConnect/Commands/BaseCommand.java
+++ src/main/java/sky/mulley/DiscordConnect/Commands/BaseCommand.java
@@ -6,10 +6,12 @@ public abstract class BaseCommand {
     protected String commandName;
     protected String helpMessage;
     protected String Usage;
+    protected boolean helpViewable;
 
     public String getCommandName() { return commandName;}
     public String getHelpMessage() { return helpMessage;}
     public String getUsage() { return Usage;}
+    public boolean isHelpViewable() { return helpViewable;}
 
     public boolean executeCommand(MessageReceivedEvent e, String[] argArray) { return true; }
 }
