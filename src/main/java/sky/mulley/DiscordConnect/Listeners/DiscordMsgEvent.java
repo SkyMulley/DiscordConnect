1c58d5325dac0bf71cd1d3b5195f617c6852db90
Daniel Mulley <daniel.mulley@outlook.com>
2018-12-05 13:17:27 +0000

    Fixed up Listeners to work with new event type

--- src/main/java/sky/mulley/DiscordConnect/Listeners/DiscordMsgEvent.java
+++ src/main/java/sky/mulley/DiscordConnect/Listeners/DiscordMsgEvent.java
@@ -2,13 +2,12 @@ package sky.mulley.DiscordConnect.Listeners;
 
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
-import sx.blah.discord.handle.impl.events.guild.channel.message.MessageEvent;
-import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import sky.mulley.DiscordConnect.GeneralLogic.MsgEvent;
 
 public class DiscordMsgEvent extends Event {
     private static final HandlerList HANDLERS = new HandlerList();
-    private MessageEvent event;
-    public DiscordMsgEvent(MessageEvent event) { event = event; }
+    private MsgEvent event;
+    public DiscordMsgEvent(MsgEvent event) { event = event; }
     public HandlerList getHandlers() { return HANDLERS; }
-    public MessageEvent getMessage() { return event; }
+    public MsgEvent getMessage() { return event; }
 }
--- src/main/java/sky/mulley/DiscordConnect/Listeners/MessageListener.java
+++ src/main/java/sky/mulley/DiscordConnect/Listeners/MessageListener.java
@@ -2,27 +2,37 @@ package sky.mulley.DiscordConnect.Listeners;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
 import sky.mulley.DiscordConnect.Commands.CommandCore;
-import sky.mulley.DiscordConnect.GeneralLogic.MessageEvent;
+import sky.mulley.DiscordConnect.GeneralLogic.MsgEvent;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
 import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IRole;
 
 public class MessageListener {
     private CommandCore cc;
     private IChannel channel = null;
-    private MessageEvent eventMsg;
+    private MsgEvent eventMsg;
+    private IRole role;
     DiscordMsgEvent msgEvent = new DiscordMsgEvent(eventMsg);
 
     public MessageListener(CommandCore cc) { this.cc =cc;}
 
     @EventSubscriber
     public void onMessageRecieved(MessageReceivedEvent event) {
-        eventMsg = (MessageEvent) event;
+        eventMsg = new MsgEvent(event.getMessage());
         if(!cc.commandCheck(eventMsg)) {
             Bukkit.getPluginManager().callEvent(msgEvent);
-        } if (channel!=null && event.getChannel().equals(channel)) {
-            Bukkit.getServer().broadcastMessage(ChatColor.BLUE+"[Discord] "+ChatColor.WHITE+event.getAuthor().getName()+": "+event.getMessage().toString());
+        } else if (channel!=null && event.getChannel().equals(channel)) {
+            if(!event.getAuthor().isBot()) {
+                if (event.getAuthor().getRolesForGuild(event.getGuild()).size() != 0) {
+                    IRole role = event.getAuthor().getRolesForGuild(event.getGuild()).get(0);
+                    Bukkit.getServer().broadcastMessage(ChatColor.BLUE + "[Discord] " + ChatColor.WHITE + event.getAuthor().getName() + " | " + role + ": " + event.getMessage().toString());
+                } else {
+                    Bukkit.getServer().broadcastMessage(ChatColor.BLUE + "[Discord] " + ChatColor.WHITE + event.getAuthor().getName()+": " + event.getMessage().toString());
+                }
+            }
         }
     }
 
