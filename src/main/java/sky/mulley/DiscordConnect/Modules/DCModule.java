7130b49fdade6dbe57c30a26f026c9160cf54083
Daniel Mulley <daniel.mulley@outlook.com>
2018-12-05 15:00:59 +0000

    Changed order and added info

--- src/main/java/sky/mulley/DiscordConnect/Modules/DCModule.java
+++ src/main/java/sky/mulley/DiscordConnect/Modules/DCModule.java
@@ -1,6 +1,7 @@
 package sky.mulley.DiscordConnect.Modules;
 
 import sky.mulley.DiscordConnect.Commands.General.Help;
+import sky.mulley.DiscordConnect.Commands.General.Info;
 import sky.mulley.DiscordConnect.Commands.General.Ping;
 import sky.mulley.DiscordConnect.Commands.General.Status;
 
@@ -8,7 +9,8 @@ public class DCModule extends DiscordModule {
     public DCModule() {
         super("DCModule","Sky");
         addCommand(new Ping());
-        addCommand(new Help(getCommands()));
         addCommand(new Status());
+        addCommand(new Info());
+        addCommand(new Help(getCommands()));
     }
 }
