10d1ba12f817b535c110e4516abccebd9f47366d
Daniel Mulley <daniel.mulley@outlook.com>
2018-12-05 13:17:06 +0000

    Added a status command as default

--- src/main/java/sky/mulley/DiscordConnect/Modules/DCModule.java
+++ src/main/java/sky/mulley/DiscordConnect/Modules/DCModule.java
@@ -2,11 +2,13 @@ package sky.mulley.DiscordConnect.Modules;
 
 import sky.mulley.DiscordConnect.Commands.General.Help;
 import sky.mulley.DiscordConnect.Commands.General.Ping;
+import sky.mulley.DiscordConnect.Commands.General.Status;
 
 public class DCModule extends DiscordModule {
     public DCModule() {
         super("DCModule","Sky");
         addCommand(new Ping());
         addCommand(new Help(getCommands()));
+        addCommand(new Status());
     }
 }
