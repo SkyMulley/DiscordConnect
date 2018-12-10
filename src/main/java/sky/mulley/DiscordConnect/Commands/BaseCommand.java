cd8506aa8d81e8c7b845eefd2deaa6f03853bf92
SkySmith2 <daniel.mulley@Outlook.com>
2018-12-10 14:24:55 +0000

    Added ability to disable commands (now redundant)

--- src/main/java/sky/mulley/DiscordConnect/Commands/BaseCommand.java
+++ src/main/java/sky/mulley/DiscordConnect/Commands/BaseCommand.java
@@ -1,13 +1,14 @@
 package sky.mulley.DiscordConnect.Commands;
 
 import sky.mulley.DiscordConnect.GeneralLogic.MsgEvent;
+import sky.mulley.DiscordConnect.Modules.DiscordModule;
 
 public abstract class BaseCommand {
     protected String commandName;
     protected String helpMessage = null;
     protected String Usage = null;
     protected boolean helpViewable = true;
-    protected String commandModule;
+    protected DiscordModule commandModule;
     public BaseCommand(String commandName) {
         this.commandName = commandName;
     }
@@ -15,8 +16,10 @@ public abstract class BaseCommand {
     public String getCommandName() { return commandName;}
     public String getHelpMessage() { return helpMessage;}
     public String getUsage() { return Usage;}
-    public String getModule() { return commandModule;}
+    public DiscordModule getModule() { return commandModule;}
     public boolean isHelpViewable() { return helpViewable;}
 
     public boolean executeCommand(MsgEvent e, String[] argArray) { return true; }
+
+    public void setModule(DiscordModule module) {this.commandModule = module;}
 }
