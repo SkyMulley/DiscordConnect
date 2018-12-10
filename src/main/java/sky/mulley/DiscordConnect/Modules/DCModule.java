d99e560123c06fe09d65f0ef92d10d164867c4bd
SkySmith2 <daniel.mulley@Outlook.com>
2018-12-10 14:23:57 +0000

    Added some module shit

--- src/main/java/sky/mulley/DiscordConnect/Modules/DCModule.java
+++ src/main/java/sky/mulley/DiscordConnect/Modules/DCModule.java
@@ -1,16 +1,17 @@
 package sky.mulley.DiscordConnect.Modules;
 
-import sky.mulley.DiscordConnect.Commands.General.Help;
-import sky.mulley.DiscordConnect.Commands.General.Info;
-import sky.mulley.DiscordConnect.Commands.General.Ping;
-import sky.mulley.DiscordConnect.Commands.General.Status;
+import org.bukkit.Bukkit;
+import sky.mulley.DiscordConnect.Commands.General.*;
+import sky.mulley.DiscordConnect.DiscordConnect;
 
 public class DCModule extends DiscordModule {
+    private DiscordConnect main = (DiscordConnect) Bukkit.getPluginManager().getPlugin("DiscordConnect");
     public DCModule() {
         super("DCModule","Sky");
         addCommand(new Ping());
         addCommand(new Status());
         addCommand(new Info());
+        addCommand(new Modules(main.getModuleManager().getModules()));
         addCommand(new Help(getCommands()));
     }
 }
